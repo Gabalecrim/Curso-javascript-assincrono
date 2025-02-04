5a21544a0d7e4287f0c574c6ac1a87a37d6d70f3
Gabriel Alecrim <eu@gabrielalecrim.com.br>
2025-02-04 09:35:15 -0300

    Ultima aula - Projeto final

--- scripts.js
+++ scripts.js
@@ -123,3 +123,17 @@ botaoPublicar.addEventListener("click", async (evento) => {
     console.error(`${error}`);
   }
 });
+
+const botaoDescartar = document.querySelector(".botao-descartar");
+
+botaoDescartar.addEventListener("click", (evento) => {
+  evento.preventDefault();
+
+  const formulario = document.querySelector("form");
+  formulario.reset();
+
+  imagemPrincipal.src = "./img/imagem1.png";
+  nomeDaImagem.textContent = "image_projeto.png";
+
+  listaTags.innerHTML = "";
+});
